9a1f48e1aecc5214cb189f21c58568a1a754e112
TonyTran03 <tony.tran03@hotmail.com>
2025-01-17 21:41:26 -0500

    revert to React 18.3.1

--- stopdropandroll/src/app/Home/page.jsx
+++ stopdropandroll/src/app/Home/page.jsx
@@ -1,4 +1,27 @@
 'use client';
+import { Canvas } from '@react-three/fiber';
+import { CameraControls } from '@react-three/drei';
+import { useState, useEffect } from 'react';
+
 export default function HomePage() {
-  return <>what</>;
+  const [isMobile, setIsMobile] = useState(false);
+
+  // Detect if the user is on a mobile device
+  useEffect(() => {
+    const checkMobile = () => setIsMobile(window.innerWidth <= 768);
+    checkMobile();
+    window.addEventListener('resize', checkMobile);
+    return () => window.removeEventListener('resize', checkMobile);
+  }, []);
+
+  return (
+    <>
+      <Canvas camera={{ fov: isMobile ? 160 : 150 }}>
+        <ambientLight intensity={1.5} />
+        <spotLight position={[0, 0, 0]} />
+        <pointLight position={[-10, -10, -10]} />
+        hello
+      </Canvas>
+    </>
+  );
 }
