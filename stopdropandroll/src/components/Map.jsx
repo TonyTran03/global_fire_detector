ef2f4e402e1d6a8f73aba9d2cab909b9e8944e3b
Zhen <zhenliu200677@gmail.com>
2025-01-17 22:38:27 -0500

    add map

---
+++ stopdropandroll/src/components/Map.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { useState } from "react";
+import Map, { Marker, NavigationControl } from "react-map-gl";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFire } from "@fortawesome/free-solid-svg-icons";
+import "mapbox-gl/dist/mapbox-gl.css";
+
+const MapComponent = () => {
+  const [viewState, setViewState] = useState({
+    latitude: 45.4211,
+    longitude: -75.6903,
+    zoom: 8,
+  });
+
+  if (!process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN) {
+    return <div>Error: Mapbox access token not found</div>;
+  }
+
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <Map
+        {...viewState}
+        style={{ width: "100%", height: "100%" }}
+        mapStyle="mapbox://styles/mapbox/streets-v12"
+        mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
+        onMove={(evt) => setViewState(evt.viewState)}
+      >
+        <NavigationControl />
+        <Marker latitude={45.4211} longitude={-75.6903}></Marker>
+      </Map>
+    </div>
+  );
+};
+
+export default MapComponent;
