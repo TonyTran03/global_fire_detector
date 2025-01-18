a92fbdde291457066697c900f7eb706e87ffee6e
Zhen <zhenliu200677@gmail.com>
2025-01-17 23:50:21 -0500

    some changes

--- stopdropandroll/src/components/Map.jsx
+++ stopdropandroll/src/components/Map.jsx
@@ -1,10 +1,8 @@
 'use client';
-import React from "react";
-import { useState } from "react";
-import Map, { Marker, NavigationControl } from "react-map-gl";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFire } from "@fortawesome/free-solid-svg-icons";
+import React, { useState } from "react";
+import Map, { NavigationControl } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
+import { BorderBeam } from "@/components/ui/border-beam.jsx";
 
 const MapComponent = () => {
   const [viewState, setViewState] = useState({
@@ -18,32 +16,49 @@ const MapComponent = () => {
   }
 
   return (
-    <div style={{ 
-        width: "100vw", 
-        height: "100vh", 
-        display: "flex", 
-        justifyContent: "center", 
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
         alignItems: "center",
-        overflow: "hidden"
-      }}>
-        <div style={{
+        overflow: "hidden",
+      }}
+    >
+      <div
+        style={{
           width: "80%",
           height: "80%",
           borderRadius: "12px",
           overflow: "hidden",
-          boxShadow: "0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)"
-        }}>
-          <Map
-            {...viewState}
-            style={{ width: "100%", height: "100%" }}
-            mapStyle="mapbox://styles/mapbox/streets-v12"
-            mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
-            onMove={evt => setViewState(evt.viewState)}
-          >
-            <NavigationControl />
-          </Map>
-        </div>
+          boxShadow:
+            "0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)",
+          position: "relative", // Ensure relative positioning for the container
+        }}
+      >
+        {/* BorderBeam placed correctly */}
+        <BorderBeam
+          size={500}
+          duration={15}
+          anchor={90}
+          borderWidth={2}
+          colorFrom="#ffaa40"
+          colorTo="#9c40ff"
+        />
+        <Map
+          {...viewState}
+          style={{ width: "100%", height: "100%" }}
+          mapStyle="mapbox://styles/mapbox/streets-v12"
+          mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
+          onMove={(evt) => {
+            setViewState(evt.viewState);
+          }}
+        >
+          <NavigationControl />
+        </Map>
       </div>
+    </div>
   );
 };
 
